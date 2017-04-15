21a34e87667aba2155a59c2d993856b7ac052eaa
but0n <but0n@icloud.com>
2017-04-15 23:42:04 +0800

    fix: Syntax erro

--- libs/source/uart.c
+++ libs/source/uart.c
@@ -113,7 +113,8 @@ void uart_num2char(unsigned short k) {
 	unsigned short bit[] = {10000, 1000, 100, 10, 1};
 
 	while(i>-1) {
-		cache[i--] += (char)(k / bit[i] % 10);
+		cache[i] += (char)(k / bit[i] % 10);
+		i--;
 	}
 	uart_sendStr(cache);
 }
