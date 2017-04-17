82362f98da63ca63140f6a5ec584abc3e09dc24f
but0n <but0n@icloud.com>
2017-04-17 19:51:21 +0800

    doc: Add comments

--- libs/include/irda.h
+++ libs/include/irda.h
@@ -14,11 +14,10 @@
 #define IR3 BIT_ADDR(TIM3->CCER, TIM_CCER_CC3E)
 #define IR4 BIT_ADDR(TIM3->CCER, TIM_CCER_CC4E)
 
-#define WAVE_SEGEMENT_LENGTH	100000
-
+#define WAVE_SEGEMENT_LENGTH	9999	//该值会影响学码函数的耗时, 因为最后一个电平的计时器会一直累加到该值函数才会结束
 // 红外学码外设对象定义
 #define IR_DEVICES_NUM	8	//共有8路学码发码外设
-#define TOKEN_LEN		400	//红外波形共有10次电平翻转
+#define TOKEN_LEN		400	//红外波形共有200次电平翻转
 typedef struct {
 	unsigned int token[TOKEN_LEN];
 	volatile unsigned long *IrInterrup;	//this reg will be a Pointer to EXTI->IMR, for enable or disable
