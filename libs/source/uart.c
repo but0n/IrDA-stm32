53f22f28118f054741a734f1ae1041ff01a8f20e
but0n <but0n@icloud.com>
2017-05-22 18:05:47 +0800

    doc: (comments)

--- libs/source/uart.c
+++ libs/source/uart.c
@@ -14,23 +14,23 @@ void uart_init(unsigned int pclk2, unsigned int bound) {
     fraction = (temp - mantissa) * 16;
     mantissa <<= 4;
     mantissa += fraction;
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;   //GPIOA Enable
-    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;  //USART1 Enable
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;		// 打开 GPIOA 时钟
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;	// 打开 USART1 时钟
 
-    GPIOA->CRH &= 0xFFFFF00F;
+    GPIOA->CRH &= 0xFFFFF00F;				// USART GPIO 引脚配置
     GPIOA->CRH |= 0x000008B0;
 
     RCC->APB2RSTR |= RCC_APB2RSTR_USART1RST;
     RCC->APB2RSTR &= ~RCC_APB2RSTR_USART1RST;
 
     USART1->BRR = mantissa;
-    USART1->CR1 |= 0x200C;	//Set up UE, TE and RE
+    USART1->CR1 |= 0x200C;	//设置 UE, TE and RE
 
-    USART1->CR1 |= 1<<8;	//PE Interrup Enable
-    USART1->CR1 |= 1<<5;	//RX Not Empty Interrup Enable
+    USART1->CR1 |= 1<<8;	//PE 中断使能
+    USART1->CR1 |= 1<<5;	//RX 非空中断使能
 
 
-    USART1->SR;     //Read Reg SR to Clean TXE and TE,(Reset value: 0x00C0)
+    USART1->SR;     // 读取 SR 寄存器以将 TXE 和 TE 位清零,(复位值: 0x00C0)
 
 	// SCB->AIRCR &= 0x05FAF8FF;	// AIRCE Key: 0x05FA
 	// SCB->AIRCR |= 0x05FA0400;	// Set up group value
@@ -104,7 +104,7 @@ void uart_sendStr(char *cmd) {
 }
 
 void uart_int2char(unsigned int k) {
-	char cache[] = "0000000000";	// Max value is 4294967295
+	char cache[] = "0000000000";	// 最大值为 4294967295
 	unsigned char i = 9;
 	const unsigned int bit[] = {1000000000, 100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1};
 
@@ -115,7 +115,7 @@ void uart_int2char(unsigned int k) {
 }
 
 void uart_short2char(unsigned short k) {
-	char cache[] = "00000";	// Max value is 4294967295
+	char cache[] = "00000";	// 最大值为 4294967295
 	unsigned char i = 4;
 	const unsigned int bit[] = {10000, 1000, 100, 10, 1};
 
